45e0ea3bbedec1eba2f70f2752a6b91fe7b5551b
Artem Novikov <novartole@gmail.com>
2024-05-28 00:42:31 +0300

    handle missing cmd

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,9 @@ fn main() {
     print!("$ ");
     io::stdout().flush().unwrap();
 
-    // Wait for user input
     let stdin = io::stdin();
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
+
+    print!("{}: command not found\n", input.trim_end());
 }
