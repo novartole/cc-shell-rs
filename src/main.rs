78aa0564aa172f4a78b79fd460b93209186e0e3f
Artem Novikov <novartole@gmail.com>
2024-05-28 01:10:30 +0300

    added REPL

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,28 @@
 use std::io::{self, Write};
 
 fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
-
-    let stdin = io::stdin();
     let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
 
-    print!("{}: command not found\n", input.trim_end());
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+
+        let cmd = read(&mut input);
+
+        if let Err(e) = eval(cmd) {
+            println!("{}", e);
+        }
+    }
+}
+
+fn read(input: &mut String) -> &str {
+    let stdin = io::stdin();
+
+    stdin.read_line(input).unwrap();
+
+    input.trim_end()
+}
+
+fn eval(cmd: &str) -> Result<String, Box<dyn std::error::Error>> {
+    Err(format!("{}: command not found", cmd).into())
 }
