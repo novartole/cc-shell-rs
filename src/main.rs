fc9c3b87067f9777477058d51c56be508b6d1d24
Artem Novikov <novartole@gmail.com>
2024-06-01 00:46:14 +0300

    handle programs with args

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,69 @@
 use std::{
     env, error, fs,
     io::{self, Write},
-    process,
+    path, process,
+    str::from_utf8,
 };
 
 use thiserror::Error;
 
-enum Cmd<'input> {
-    Exit(i32),
-    Echo(&'input str),
-    Type(&'input str),
-}
-
 #[derive(Debug, Error)]
 enum AppError {
-    #[error("command not found")]
-    CmdNotFound,
     #[error("{0}")]
     BadArg(&'static str),
     #[error(transparent)]
     Io(#[from] io::Error),
 }
 
+enum Cmd<'input> {
+    Exit(i32),
+    Echo(&'input str),
+    Type(&'input str),
+    External {
+        cmd: &'input str,
+        args: Option<&'input str>,
+    },
+}
+
 impl<'input> TryFrom<&'input str> for Cmd<'input> {
     type Error = AppError;
 
     fn try_from(input: &'input str) -> Result<Self, Self::Error> {
-        use AppError::{BadArg, CmdNotFound};
+        use AppError::BadArg;
 
-        match input.split_once(' ') {
-            Some((cmd, args)) => {
-                let cmd = match cmd {
-                    "exit" => {
-                        let arg = args.trim();
+        let cmd = match input.split_once(' ') {
+            Some((cmd, args)) => match cmd {
+                "exit" => {
+                    let arg = args.trim_start();
 
-                        if arg.is_empty() {
-                            return Err(BadArg("exit code is required"));
-                        }
-
-                        let code = arg
-                            .parse()
-                            .map_err(|_| BadArg("failed to parse exit code"))?;
-
-                        Cmd::Exit(code)
+                    if arg.is_empty() {
+                        return Err(BadArg("exit code is required"));
                     }
-                    "echo" => Cmd::Echo(args),
-                    "type" => {
-                        let cmd = args.trim();
 
-                        Cmd::Type(cmd)
-                    }
-                    _ => return Err(CmdNotFound),
-                };
+                    let code = arg
+                        .parse()
+                        .map_err(|_| BadArg("failed to parse exit code"))?;
 
-                Ok(cmd)
-            }
-            None => Err(CmdNotFound),
-        }
+                    Cmd::Exit(code)
+                }
+                "echo" => Cmd::Echo(args),
+                "type" => {
+                    let cmd = args.trim_start();
+
+                    Cmd::Type(cmd)
+                }
+                cmd => Cmd::External {
+                    cmd,
+                    args: Some(args),
+                },
+            },
+            None => Cmd::External {
+                cmd: input,
+                args: None,
+            },
+        };
+
+        Ok(cmd)
     }
 }
 
@@ -80,17 +87,7 @@ fn run() -> Result<(), Box<dyn error::Error>> {
 
         // parse
         let input = buf.trim_end();
-        let cmd = match input.try_into() {
-            Ok(cmd) => cmd,
-            Err(e) => {
-                if let AppError::CmdNotFound = e {
-                    println!("{}: {}", input, e);
-                    continue;
-                }
-
-                return Err(e.into());
-            }
-        };
+        let cmd = input.try_into()?;
 
         // eval
         match cmd {
@@ -105,11 +102,7 @@ fn run() -> Result<(), Box<dyn error::Error>> {
                 if let Ok(val) = env::var("PATH") {
                     for dir in val.split(':') {
                         for entry in fs::read_dir(dir)? {
-                            let entry = entry?;
-
-                            if entry.file_type().as_ref().is_ok_and(fs::FileType::is_file)
-                                && entry.file_name() == cmd
-                            {
+                            if entry?.file_name() == cmd {
                                 println!("{0} is {1}/{0}", cmd, dir);
                                 continue 'repl;
                             }
@@ -117,7 +110,50 @@ fn run() -> Result<(), Box<dyn error::Error>> {
                     }
                 }
 
-                println!("{} not found", cmd,)
+                println!("{} not found", cmd)
+            }
+            Cmd::External {
+                cmd,
+                args: raw_args,
+            } => {
+                let path = path::Path::new(cmd);
+
+                let cmd = match path.try_exists() {
+                    Ok(true) => path.into(),
+                    _ => {
+                        let mut path = None;
+
+                        if let Ok(val) = env::var("PATH") {
+                            'outer: for dir in val.split(':') {
+                                for entry in fs::read_dir(dir)? {
+                                    let entry = entry?;
+
+                                    if entry.file_name() == cmd {
+                                        path = entry.path().into();
+                                        break 'outer;
+                                    }
+                                }
+                            }
+                        }
+
+                        match path {
+                            Some(cmd) => cmd,
+                            None => {
+                                println!("{}: command not found", input);
+                                continue 'repl;
+                            }
+                        }
+                    }
+                };
+
+                let mut prc = process::Command::new(cmd);
+
+                if let Some(args) = raw_args {
+                    prc.args(args.split_whitespace().filter(|arg| !arg.is_empty()));
+                }
+
+                let output = prc.output()?.stdout;
+                print!("{}", from_utf8(&output)?);
             }
         }
     }
